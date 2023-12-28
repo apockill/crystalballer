b6461b62a0115a770c2eb9880aae416f391f9161
Alex Thiele <alex@urbanmachine.build>
2023-12-27 22:45:21 -0800

    Initial commit

---
+++ crystalballer/scripts/run_oakd.py
@@ -0,0 +1,12 @@
+"""A helper script for connecting to the Oak-D and visualizing output"""
+
+
+import argparse
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+
+
+if __name__ == "__main__":
+    main()
