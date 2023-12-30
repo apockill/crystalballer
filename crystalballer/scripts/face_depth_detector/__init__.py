3f7fae902424c7762db5d4fce57883650158ed10
Alex Thiele <alex@urbanmachine.build>
2023-12-30 12:14:41 -0800

    First time the face position tracker works properly!

---
+++ crystalballer/scripts/face_depth_detector/__init__.py
@@ -0,0 +1,39 @@
+import cv2
+
+from crystalballer.depthai_pipelines import FacePositionPipeline
+
+from .drawing import TextHelper
+from .inference import StereoInference
+
+
+def main():
+    face_pipeline = FacePositionPipeline()
+
+    # Pipeline is defined, now we can connect to the device
+    with face_pipeline:  # tODO: Used to check openvino version
+        # Set device log level - to see logs from the Script node
+        text_helper = TextHelper()
+
+        while True:
+            # 300x300 Mono image frames
+            left_frame = face_pipeline.left_frame_queue.get().getCvFrame()
+            right_frame = face_pipeline.right_frame_queue.get().getCvFrame()
+
+            # Combine the two mono frames
+            combined = cv2.addWeighted(left_frame, 0.5, right_frame, 0.5, 0)
+
+            # 3D visualization
+            face_detection = face_pipeline.latest_face_detection
+
+            if face_detection is not None:
+                print("OWO", face_detection)
+                strings = [
+                    "X: {:.2f} m".format(face_detection.centroid[0]),
+                    "Y: {:.2f} m".format(face_detection.centroid[1]),
+                    "Z: {:.2f} m".format(face_detection.centroid[2]),
+                ]
+                text_helper.draw_text(combined, strings, (10, 10))
+
+            cv2.imshow("Combined stereo", combined)
+            if cv2.waitKey(1) == ord("q"):
+                break
