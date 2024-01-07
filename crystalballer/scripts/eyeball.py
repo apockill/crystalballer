Fix scaling bug

crystalballer/scripts/eyeball.py
@@ -3,11 +3,10 @@ import numpy as np
 import open3d as o3d
 
 from crystalballer import constants
-from crystalballer.constants import GAKKEN_RADIUS
 from crystalballer.o3d_utils import VirtualCameraRenderer, Visualizer
 from crystalballer.resources import GLOBE
 
-PROJECTOR_DISTANCE = GAKKEN_RADIUS * 25.5
+PROJECTOR_DISTANCE = constants.GAKKEN_RADIUS * 2.67
 """Distance from the projector to the center of the globe in meters, with the default
 open3d camera intrinsics
 """
@@ -23,7 +22,7 @@ def main() -> None:
     visualizer = Visualizer()
     visualizer.set_camera(
         eye=(0, 0, 0),
-        center=(0, 0, -constants.PROJECTOR_DISTANCE),
+        center=(0, 0, -PROJECTOR_DISTANCE),
         up=(0, -1, 0),
     )
 
@@ -33,7 +32,7 @@ def main() -> None:
     ]
     while True:
         camera_position = np.eye(4)
-        camera_position[:3, 3] = [0, 0, constants.PROJECTOR_DISTANCE]
+        camera_position[:3, 3] = [0, 0, PROJECTOR_DISTANCE]
 
         color, depth = renderer.render(camera_position, geometries)
         cv2.imshow("Color", color)