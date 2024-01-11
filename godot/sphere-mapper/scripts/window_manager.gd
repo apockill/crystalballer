9a699df1c90f8ea93c2fe0a00edcc6a174238c47
Ryan Hedgecock <rhedgeco@gmail.com>
2024-01-11 10:29:06 -0800

    added fullscreen toggle

---
+++ godot/sphere-mapper/scripts/window_manager.gd
@@ -0,0 +1,11 @@
+extends Node
+
+func _process(delta):
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		toggle_fullscreen()
+
+func toggle_fullscreen():
+	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
