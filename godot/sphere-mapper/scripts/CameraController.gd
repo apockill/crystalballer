8a5f4702d7c25f96c67b247b7917a11ec7e67578
Ryan Hedgecock <rhedgeco@gmail.com>
2024-01-12 10:54:44 -0800

    rename camera_controller and fix FOV

---
+++ godot/sphere-mapper/scripts/CameraController.gd
@@ -0,0 +1,13 @@
+@tool
+extends Camera3D
+
+@export var camera_target: Node3D
+@export var gakken_radius: float = 0.5;
+
+func _process(_delta):
+	look_at(camera_target.global_position)
+	
+	# calculate fov so that it is tangent to the spheres surface
+	# https://www.desmos.com/calculator/vuxczxsxqg
+	var distance = (camera_target.global_position - global_position).length()
+	fov = 2 * rad_to_deg(acos(sqrt(distance**2 - gakken_radius**2) / distance))
