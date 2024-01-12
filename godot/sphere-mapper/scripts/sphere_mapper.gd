1e074f66b13753b029f6a7c02d4b5a3b1de90379
Ryan Hedgecock <rhedgeco@gmail.com>
2024-01-11 17:58:24 -0800

    fixed sphere projection mapping

--- godot/sphere-mapper/scripts/sphere_mapper.gd
+++ godot/sphere-mapper/scripts/sphere_mapper.gd
@@ -1,10 +1,15 @@
+@tool
 extends ColorRect
 
 @export var gakken: Node3D
 @export var camera: Camera3D
 
 
-func _process(delta):
+func _process(_delta):
+	var mvp_matrix = camera.get_camera_projection();
+	mvp_matrix *= Projection(camera.get_camera_transform()).inverse();
+	mvp_matrix *= Projection(gakken.transform);
+	
+	# set shader mvp_matrix
 	var shadermat = material as ShaderMaterial
-	shadermat.set_shader_parameter("camera_up", camera.basis.y)
-	shadermat.set_shader_parameter("camera_right", camera.basis.x)
+	shadermat.set_shader_parameter("mvp_matrix", mvp_matrix)
