c93cdc2302e9a1716e88ed9d4817cf695ae25f17
Ryan Hedgecock <rhedgeco@gmail.com>
2024-01-12 11:14:20 -0800

    add tracking manager with placeholder functions

---
+++ godot/sphere-mapper/scripts/TrackingManager.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@export var gakken: Node3D
+@export var camera: Node3D
+
+var relative_pos: Vector3
+
+func _ready():
+	relative_pos = gakken.position - camera.position
+	# TODO: Init necessary camera tracking server/hardware
+
+func _process(delta):
+	relative_pos = get_relative_camera_position()
+	camera.position = gakken.position + relative_pos
+
+func get_relative_camera_position() -> Vector3:
+	# TODO: get relative position from tracking camera
+	return relative_pos # TODO: and remove this line
