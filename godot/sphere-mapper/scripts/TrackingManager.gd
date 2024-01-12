a3f1ef7dc4b3af41a8973230d3a92f7a679a0a61
Ryan Hedgecock <rhedgeco@gmail.com>
2024-01-12 11:37:55 -0800

    remove placeholder code from tracker

--- godot/sphere-mapper/scripts/TrackingManager.gd
+++ godot/sphere-mapper/scripts/TrackingManager.gd
@@ -3,16 +3,11 @@ extends Node
 @export var gakken: Node3D
 @export var camera: Node3D
 
-var relative_pos: Vector3
-
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	relative_pos = camera.position - gakken.position;
-	# TODO: Init necessary camera tracking server/hardware
+	pass # Replace with function body.
 
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	relative_pos = get_relative_camera_position()
-	camera.position = gakken.position + relative_pos
-
-func get_relative_camera_position() -> Vector3:
-	# TODO: get relative position from tracking camera
-	return relative_pos # TODO: and remove this line
+	pass
